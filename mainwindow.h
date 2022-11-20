12adf3ae886b1c668ecf41d8b82e02dc977ec4f8
Spencer <spencerlow1@outlook.com>
2022-11-20 16:57:15 -0500

    Project default files
    Qt Widgets Application template

---
+++ mainwindow.h
@@ -0,0 +1,48 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QElapsedTimer>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+
+    //
+    //include "defs.h"
+    QTimer* t;
+    QElapsedTimer* et;
+    int deviceStatus = OFF; //using defs.h
+    void initTimer();
+    void initConnections();
+    void drainBattery();
+    void temp();
+    //
+
+private:
+    Ui::MainWindow *ui;
+};
+
+private slots:
+
+    void powerPress();
+    void powerRelease();
+
+    void upPress();
+    void downPress();
+    void confirmPress();
+
+    void toggleLeftEar();
+    void toggleRightEar();
+
+    void update();
+#endif // MAINWINDOW_H
